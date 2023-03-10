b0ac3720d1c68bfe4f61147cc61a3f51827511a1
kinoshita kazuoki <kino05040826@gmail.com>
2023-03-10 16:38:08 +0900

    blogモデル、blogsテーブル作成

---
+++ db/schema.rb
@@ -0,0 +1,12 @@
+ActiveRecord::Schema.define(version: 2023_03_10_073516) do
+
+  enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
